12b25b2bf4eb244839e9f7b59addbd5836b4a7b8
Onni <onni.alasaari@metropolia.fi>
2022-11-24 11:35:28 +0200

    testi commit

--- app/src/main/java/com/example/ravintosovellushyte2022/MainActivity.java
+++ app/src/main/java/com/example/ravintosovellushyte2022/MainActivity.java
@@ -11,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Onnin kommentti, kirjoitettu 24.11.22 @11:33
 }
